97153abea62479b98a5cc4b6bb35e3cfb04bc3b2
BrunoPoianoVlcode <bruno.poiano@vlcode.com.br>
2025-02-21 18:43:04 -0300

    creating docker file to frontend

--- frontend/src/eventBus/index.ts
+++ frontend/src/eventBus/index.ts
@@ -1,19 +1,20 @@
 import { reactive } from 'vue'
+type EventCallback = (...args:unknown[])=>void
 
 const eventBus = reactive({
-  events: {},
-  $on(event, callback) {
+  events: {} as Record<string, EventCallback[]>,
+  $on(event:string, callback:EventCallback) {
     if (!this.events[event]) {
       this.events[event] = []
     }
     this.events[event].push(callback)
   },
-  $emit(event, ...args) {
+  $emit(event:string, ...args:unknown[]) {
     if (this.events[event]) {
       this.events[event].forEach((callback) => callback(...args))
     }
   },
-  $off(event, callback) {
+  $off(event:string, callback:EventCallback) {
     if (!this.events[event]) return
     this.events[event] = this.events[event].filter((cb) => cb !== callback)
   },
